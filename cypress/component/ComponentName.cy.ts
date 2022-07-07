557aa8d992877d5e2856f715fd785cee861a595a
Sara Regan <sararegan@Saras-MacBook-Pro.local>
2022-07-07 09:04:23 +0200

    feat: create-vue app via cli and setup cypress

---
+++ cypress/component/ComponentName.cy.ts
@@ -0,0 +1,7 @@
+import {mount} from 'cypress/vue'
+import ComponentName from './ComponentName.vue'
+describe('ComponentName.cy.ts', () => {
+  it('playground', () => {
+    mount(ComponentName)
+  })
+})
---
+++ cypress/component/ComponentName.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      count: 1
+    }
+  }
+})
+</script>
+
+<template>
+  // <!-- type checking and auto-completion enabled -->
+  {{ count.toFixed(2) }}
+</template>
