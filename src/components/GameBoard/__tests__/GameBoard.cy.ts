6c7aeff28f7641d6238df45d19301904e789d56c
sareg0 <1307818+sareg0@users.noreply.github.com>
2022-07-26 09:21:29 +0200

    Feat/implement game modules (#11)
    * feat: utilise chessjs for board layout
    
    * feat: print out the piece at the space

--- src/components/GameBoard/__tests__/GameBoard.cy.ts
+++ src/components/GameBoard/__tests__/GameBoard.cy.ts
@@ -4,16 +4,10 @@ const moveButton = "[data-testid=moveButton]";
 const piece = "[data-testid=piece]";
 const board = "[data-testid=board]";
 describe("GameBoard.cy.ts", () => {
-  it("makes a move", () => {
+  it("renders a board with the correct pieces", () => {
     mount(GameBoard);
     cy.get(board).should("be.visible");
     // board should have 'piece'
-    cy.get(piece).should("have.text", "piece0");
-    cy.get(moveButton).click();
-    // after clicking the piece should have 'moved'
-    cy.get(piece).should("have.text", "piece5");
-    cy.get(moveButton).click();
-    // after clicking again the piece should have 'moved' back
-    cy.get(piece).should("have.text", "piece0");
+    cy.get(piece).should("be.visible");
   });
 });
